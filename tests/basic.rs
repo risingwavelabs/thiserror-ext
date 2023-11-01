b7bab6f787a6eafcc6de6fc005cf066b92d5f1ff
Bugen Zhao <i@bugenzhao.com>
2023-11-01 19:27:39 +0800

    initial commit
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

---
+++ tests/basic.rs
@@ -0,0 +1,37 @@
+#![feature(error_generic_member_access)]
+
+use std::backtrace::Backtrace;
+use thiserror::*;
+use thiserror_ext::*;
+
+#[derive(Error, Debug, Constructor, ResultExt, Box)]
+#[thiserror_ext(type = MyError, backtrace)]
+pub enum MyErrorInner {
+    #[error("cannot parse int from `{from}`")]
+    Parse {
+        #[source]
+        error: std::num::ParseIntError,
+        from: String,
+    },
+
+    #[error("cannot parse int")]
+    ParseUnnamed(#[source] std::num::ParseFloatError, String),
+
+    #[error("cannot parse int")]
+    ParseWithBacktrace {
+        #[source]
+        error: std::num::ParseIntError,
+        backtrace: Backtrace,
+    },
+
+    #[error("cannot parse int from `{from}`")]
+    ParseWithBacktraceAndContext {
+        #[source]
+        error: std::num::ParseIntError,
+        backtrace: Backtrace,
+        from: String,
+    },
+
+    #[error(transparent)]
+    IoTransparent(std::io::Error),
+}
